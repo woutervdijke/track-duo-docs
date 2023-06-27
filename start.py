9cd0546735064ee2779a04fc1017847927147530
Wouter van Dijke <wouter@woutervandijke.nl>
2023-06-27 11:35:23 +0200

    Update start.py

--- start.py
+++ start.py
@@ -3,7 +3,8 @@ import http.client
 conn = http.client.HTTPSConnection("www.rijksoverheid.nl")
 
 headers = {
-       }
+    'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
 
 conn.request("GET", "/documenten?trefwoord=duo&startdatum=&einddatum=&onderdeel=Ministerie%20van%20Onderwijs%2C%20Cultuur%20en%20Wetenschap&type=Alle%20documenten", headers=headers)
 
